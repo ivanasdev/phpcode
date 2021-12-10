815f4bf34aafc85a80a454d8fd9047db87dbe21d
ivanasdev <91766331+ivanasdev@users.noreply.github.com>
2021-12-10 11:14:47 -0600

    Create uploader.php
    works with upform.php

---
+++ uploader.php
@@ -0,0 +1,42 @@
+<?php
+$user='IAN';
+$fecha=date('ymd-h-i-s');
+	
+	//Como el elemento es un arreglos utilizamos foreach para extraer todos los valores
+	foreach($_FILES["archivo"]['tmp_name'] as $key => $tmp_name)
+	{
+		//Validamos que el archivo exista
+
+		if($_FILES["archivo"]["name"][$key]) {
+            //Obtenemos el nombre original del archivo
+			$filename = $_FILES["archivo"]["name"][$key]; 
+             //Obtenemos un nombre temporal del archivo
+			$source = $_FILES["archivo"]["tmp_name"][$key];
+            //Declaramos un  variable con la ruta donde guardaremos los archivos, tambien  ahí se concatenan el nombre del ususario y la fecha para su identificacion.
+			$directorio = 'docs/'.$user.$fecha; 
+			//Validamos si la ruta de destino existe, en caso de no existir la creamos
+			if(!file_exists($directorio)){
+				mkdir($directorio, 0777) or die("No se puede crear el directorio de extracci&oacute;n");	
+			}
+
+            //Extension del arhivo
+            $extension=explode(".",$filename);
+            $var = $extension;
+            $extencionprue= end($var);
+
+			$newname="Proveedocs_".$user."_".$fecha.".".$extencionprue;
+
+			$dir=opendir($directorio); //Abrimos el directorio de destino
+			$target_path = $directorio.'/'.$newname; //Indicamos la ruta de destino, así como el nombre del archivo
+			
+			//Movemos y validamos que el archivo se haya cargado correctamente
+			//El primer campo es el origen y el segundo el destino
+			if(move_uploaded_file($source, $target_path)) {	
+				echo "El archivo $filename se ha almacenado en forma exitosa.<br>";
+				} else {	
+				echo "Ha ocurrido un error, por favor inténtelo de nuevo.<br>";
+			}
+			closedir($dir); //Cerramos el directorio de destino
+		}
+	}
+?>
